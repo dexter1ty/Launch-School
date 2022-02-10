23cb3666e5a88c20c1bba960562ad100b254aa82
dexter1ty <comptes@engelberts.ch>
2022-02-11 00:22:03 +0100

    Another commit

--- Core/JS101-lesson2/2.5-calculator.js
+++ Core/JS101-lesson2/2.5-calculator.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+const  MESSAGES = require('./calculator-messages.json');
 
 function prompt(text) {
   console.log(`=> ${text}`);
@@ -8,47 +9,58 @@ function unvalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
-prompt("Welcome to the Calculator!");
+prompt(MESSAGES['welcome']);
 
-prompt("Tell me the 1st number");
-let num1 = readline.question();
+while (true) {
 
-while (unvalidNumber(num1)) {
-  prompt("It's not a valid number. Please enter an integer");
-  num1 = readline.question();
-}
+  prompt(MESSAGES['num1']);
+  let num1 = readline.question();
 
-prompt("Tell me the 2nd number");
-let num2 = readline.question();
+  while (unvalidNumber(num1)) {
+    prompt(MESSAGES['notValidNum']);
+    num1 = readline.question();
+  }
 
-while (unvalidNumber(num2)) {
-  prompt("It's not a valid number. Please enter an integer");
-  num2 = readline.question();
-}
+  prompt(MESSAGES['num2']);
+  let num2 = readline.question();
 
-prompt("What operation would you like to perform?\n1) Add 2) Substract 3) Multiply 4) Divide");
-let operation = readline.question();
+  while (unvalidNumber(num2)) {
+    prompt(MESSAGES['notValidNum']);
+    num2 = readline.question();
+  }
 
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt("You must choose between 1,2,3 or 4");
-  operation = readline.question();
-}
+  prompt(MESSAGES['operation']);
+  let operation = readline.question();
 
-let result;
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt(MESSAGES['notValidChoice']);
+    operation = readline.question();
+  }
 
-switch (operation) {
-  case '1':
-    result = Number(num1) + Number(num2);
+  let result;
+
+  switch (operation) {
+    case '1':
+      result = Number(num1) + Number(num2);
+      break;
+    case '2':
+      result = Number(num1) - Number(num2);
+      break;
+    case '3':
+      result = Number(num1) * Number(num2);
+      break;
+    default:
+      result = Number(num1) / Number(num2);
+      break;
+  }
+
+  console.log(`${MESSAGES['result']} ${result}`);
+
+  prompt(MESSAGES['another']);
+  let answer = readline.question();
+
+  if (answer !== "y") {
+    prompt(MESSAGES['bye']);
     break;
-  case '2':
-    result = Number(num1) - Number(num2);
-    break;
-  case '3':
-    result = Number(num1) * Number(num2);
-    break;
-  default:
-    result = Number(num1) / Number(num2);
-    break;
+  }
 }
-
-console.log(`The result is ${result}`);
